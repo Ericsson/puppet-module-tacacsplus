422705130cadd5f9aae28cd90ca78eed57309e58
Diddi Oscarsson <diddi.oscarsson@cyberbacon.net>
2013-09-24 08:51:09 +0200

    Initial add of module files

---
+++ manifests/init.pp
@@ -0,0 +1,39 @@
+#
+# elanop tacplus class
+# http://www.shrubbery.net/tac_plus
+#
+
+
+class tacacsplus (
+    $tacplus_pkg        = "tacacs+-F4.0.4.26-1",
+) {
+
+    if $operatingsystem =~ /RedHat|CentOS/ {
+        package { $tacplus_pkg :
+            ensure => installed,
+        }
+    } else {
+        fail ("Operating system not supported")
+    }
+
+    file { "/etc/tac_plus.conf" :
+        notify  => Service["tac_plus"],
+        owner   => root,
+        group   => root,
+        require => Package[$tacplus_pkg],
+#        content => template("tacacsplus/tac_plus.conf.erb"),
+    }
+
+    file { "/etc/pam.d/tac_plus" :
+        owner   => root,
+        group   => root,
+        require => Package[$tacplus_pkg],
+        content => template("tacacsplus/tac_plus.erb"),
+     }
+
+    service { "tac_plus" :
+        enable => true,
+        ensure => running,
+        require => File['/etc/tac_plus.conf', '/etc/pam.d/tac_plus'],
+    }
+}
